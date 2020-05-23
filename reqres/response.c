ffb6800e7fef3622d612460bfc1b52c48d133ca4
Aaron <htan6081@uni.sydney.edu.au>
2020-05-23 12:03:10 +1000

    Debug output

--- reqres/response.c
+++ reqres/response.c
@@ -12,17 +12,16 @@ void err_response(uint8_t* err) {
 
 void echo_response(int socket_fd, struct message* msg) {
   uint8_t* resp = malloc(9 + msg->payload_len);
-  resp[0] = 0x01;
+  resp[0] = msg->header;
   memcpy((resp + 1), &msg->payload_len, 8);
   memcpy((resp + 9), msg->payload, msg->payload_len);
 
+  // Convert from little endian to big endian for payload
+
   for (int i = 0; i < (9 + msg->payload_len); i++) {
-    printf("Resp message contains byte %hhx\n", resp[i]);
+    printf("%hhx", resp[i]);
   }
 
-  // write(socket_fd, &msg->header, 1);
-  // write(socket_fd, &msg->payload_len, 8);
-  // write(socket_fd, msg->payload, msg->payload_len);
   write(socket_fd, resp, 9 + msg->payload_len);
 
   free(resp);
